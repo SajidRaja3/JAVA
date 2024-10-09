58d5c143f60c8c804c0971f85e848c48f72efaeb
SajidRaja3 <sajidraja9134201371@gmail.com>
2024-10-09 12:36:13 +0000

    Added Sum Program in JAVA and CPP

--- Hello.java
+++ Hello.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!!");
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter Your 1st Number: ");
+        a=sc.nextInt();
+        System.out.print("Enter Your 2nd Number: ");
+        b=sc.nextInt();
+        System.out.print("Sum of Numbers is : ");
+        System.out.println(a+b);
     }
 }
--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"Hello WOrld\n";
+    int a,b;
+    cout<<"Enter Num 1: ";
+    cin>>a;
+    cout<<"Enter Num 2: ";
+    cin>>b;
+    cout<<"Sum of Numbers "<<a+b;
+    
 }
